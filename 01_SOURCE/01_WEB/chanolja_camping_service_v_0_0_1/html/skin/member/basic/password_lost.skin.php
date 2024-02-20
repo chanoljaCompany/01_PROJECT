8c3999c9738f5c809d0d3538ce02a5c2c7ef3e70
chkim0205 <158939143+chkim0205@users.noreply.github.com>
2024-02-20 10:43:35 +0900

    (ch.kim) 캠핑 정보찾기 페이지 수정
    캠핑
    정보찾기 요소 위치 변경 및 와이드, 색상 등 변경

--- 01_SOURCE/01_WEB/chanolja_camping_service_v_0_0_1/html/skin/member/basic/password_lost.skin.php
+++ 01_SOURCE/01_WEB/chanolja_camping_service_v_0_0_1/html/skin/member/basic/password_lost.skin.php
@@ -1,32 +1,48 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
+
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
 ?>
 
+
+<style>
+    body {
+        width:100vw !important;
+        height: 100vh !important;
+    }
+</style>
+
 <!-- 회원정보 찾기 시작 { -->
 <div id="find_info" class="new_win">
     <h1 id="win_title">회원정보 찾기</h1>
-    <div class="new_win_con">
-        <form name="fpasswordlost" action="<?php echo $action_url ?>" onsubmit="return fpasswordlost_submit(this);" method="post" autocomplete="off">
-        <fieldset id="info_fs">
-            <p>
-                회원가입 시 등록하신 이메일 주소를 입력해 주세요.<br>
-                해당 이메일로 아이디와 비밀번호 정보를 보내드립니다.
-            </p>
-            <label for="mb_email" class="sound_only">E-mail 주소<strong class="sound_only">필수</strong></label>
-            <input type="text" name="mb_email" id="mb_email" required class="required frm_input full_input email" size="30" placeholder="E-mail 주소">
-        </fieldset>
-        <?php echo captcha_html();  ?>
-
-        <div class="win_btn">
-            <button type="submit" class="btn_submit">확인</button>
-            <button type="button" onclick="window.close();" class="btn_close">창닫기</button>  
-        </div>
-        </form>
-    </div>
 </div>
+<div style="width:100%; height:100%; position: relative;">
+        <div class="new_win_con"
+            style="position: absolute; top: calc(50% - 82px); left: 50%; transform: translate(-50%,-50%);">
+            <form name="fpasswordlost" action="<?php echo $action_url ?>" onsubmit="return fpasswordlost_submit(this);" method="post" autocomplete="off"
+                style="display:flex; flex-direction:column; align-items:center;">
+            <fieldset id="info_fs" style="margin-bottom: 30px;">
+                <div style="display:flex; flex-direction:column;" >
+                    <p style="margin-bottom:20px">
+                        회원가입 시 등록하신 이메일 주소를 입력해 주세요.<br>
+                        해당 이메일로 아이디와 비밀번호 정보를 보내드립니다.
+                    </p>
+                    <label for="mb_email" class="sound_only">E-mail 주소<strong class="sound_only">필수</strong></label>
+                    <input type="text" name="mb_email" id="mb_email" required class="required frm_input full_input email" size="30" placeholder="E-mail 주소"
+                        style="background: transparent !important; border: #54C3F1 2px solid; border-radius: 20px; padding: 5px 10px;">
+                </div>
+            </fieldset>
+            <?php echo captcha_html();  ?>
+
+            <div class="win_btn" style="display:flex; justify-content: space-between; width:187px;">
+                <button style="width:85px;" type="submit" class="btn_submit">확인</button>
+                <button style="width:85px; height:40px; border-radius: 20px;" type="button" onclick="window.close();" class="btn_close">창닫기</button>
+            </div>
+            </form>
+        </div>
+    <div>
 
 <script>
 function fpasswordlost_submit(f)
