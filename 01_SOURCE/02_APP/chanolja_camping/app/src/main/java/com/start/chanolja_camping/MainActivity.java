271e84c9ff5267a4c16b6b8af4136011d3edff1d
김미나 <157570901+chanoljaCompany@users.noreply.github.com>
2024-02-21 13:06:15 +0900

    (mn.kim) 1. 안드로이드 앱 수정사항 업로드

--- 01_SOURCE/02_APP/chanolja_camping/app/src/main/java/com/start/chanolja_camping/MainActivity.java
+++ 01_SOURCE/02_APP/chanolja_camping/app/src/main/java/com/start/chanolja_camping/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, LoadingActivity.class);
         startActivity(intent);
+
+        WebView webView = null;
+        webView = (WebView) findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient()); // 새 창 띄우지 않기
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+
+            }
+        });
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("http://www.chanolja.co.kr/");
     }
 }
