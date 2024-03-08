daac2080ea697c460e39593cd6dbe4798fadf68e
김미나 <157570901+chanoljaCompany@users.noreply.github.com>
2024-03-08 13:46:15 +0900

    (mn.kim) 1. 코어 api & 관리자 페이지 새 프로젝트(코드이그나이터로 변경))

--- 01_SOURCE/01_WEB/chanolja_mng/src/main/java/chanolja/server/chanolja_mng/controller/cntAuth.java
+++
@@ -1,9 +0,0 @@
-package chanolja.server.chanolja_mng.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class cntAuth {
-
-}
