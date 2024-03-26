690dea3ba4f70c397a4d3704d9a595061a2af2dc
chkim0205 <157570901+chanoljaCompany@users.noreply.github.com>
2024-03-26 16:46:14 +0900

    (ch.kim) 렌트카기업 헤더 모바일 버전 수정
    렌트카기업 헤더 모바일 버전 수정

--- 01_SOURCE/01_WEB/chanolja_rentcar_company_v_0_0_1/html/head.php
+++ 01_SOURCE/01_WEB/chanolja_rentcar_company_v_0_0_1/html/head.php
@@ -74,19 +74,30 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
         </div>
         <div id="info">
             <div class="userBox">
-                    <ul>
-                        <?php if ($is_member) {  ?>
+                <ul id="linkContainer">
+                    <?php if ($is_member) {  ?>
                         <li><a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=<?php echo G5_BBS_URL ?>/register_form.php">정보수정</a></li>
                         <li><a href="<?php echo G5_BBS_URL ?>/logout.php">로그아웃</a></li>
                         <?php if ($is_admin) {  ?>
                         <li class="tnb_admin"><a href="<?php echo correct_goto_url(G5_ADMIN_URL); ?>">관리자</a></li>
-                        <?php }  ?>
-                        <?php } else {  ?>
-                        <li><a href="<?php echo G5_BBS_URL ?>/register.php">회원가입</a></li>
-                        <li><a href="<?php echo G5_BBS_URL ?>/login.php">로그인</a></li>
-                        <?php }  ?>
-                        <li><a href="https://www.youtube.com/@TV-ph5rs"><button style="border: none; background-color: transparent;"><img src="http://gsrent.dothome.co.kr/theme/c_rentcar/img/pngegg.png" style="width: 30px;"></button></a></li>
-                    </ul>
+                    <?php }  ?>
+                    <?php } else {  ?>
+                        <script>
+                            window.onload = function() {
+                                var linkContainer = document.getElementById('linkContainer');
+                                if (window.innerWidth <= 768) {
+                                    // 모바일 화면일 때
+                                    linkContainer.innerHTML = '<li><a href="<?php echo G5_BBS_URL ?>/register.php"><img src="http://gsrent.dothome.co.kr/theme/c_rentcar/img/회원가입아이콘.png" style="width: 30px;"></a></li><li><a href="<?php echo G5_BBS_URL ?>/login.php"><img src="http://gsrent.dothome.co.kr/theme/c_rentcar/img/로그인아이콘.png" style="width: 30px;"></a></li> <li><a href="https://www.youtube.com/@TV-ph5rs"><button style="border: none; background-color: transparent;"><img src="http://gsrent.dothome.co.kr/theme/c_rentcar/img/pngegg.png" style="width: 30px;"></button></a></li>';
+                                } else {
+                                    // 데스크탑 화면일 때
+                                    linkContainer.innerHTML = '<li><a href="<?php echo G5_BBS_URL ?>/register.php">회원가입</a></li><li><a href="<?php echo G5_BBS_URL ?>/login.php">로그인</a></li> <li><a href="https://www.youtube.com/@TV-ph5rs"><button style="border: none; background-color: transparent;"><img src="http://gsrent.dothome.co.kr/theme/c_rentcar/img/pngegg.png" style="width: 30px;"></button></a></li>';
+                                }
+                            }
+
+                        </script>
+                    <?php }  ?>
+                    <li><a href="https://www.youtube.com/@TV-ph5rs"><button style="border: none; background-color: transparent;"><img src="http://gsrent.dothome.co.kr/theme/c_rentcar/img/pngegg.png" style="width: 30px;"></button></a></li>
+                </ul>
             </div>
         </div>
         <div class="hamburger">
